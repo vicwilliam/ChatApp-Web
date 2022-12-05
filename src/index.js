d2d4a76c0734875f37b2ecf690529c09c46d69b9
Victor Paulini <vic.paulini@gmail.com>
2022-12-05 17:39:35 -0300

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createSignalRContext} from "react-signalr";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const SignalRContext = createSignalRContext();
+export const useSignalREffect = SignalRContext.useSignalREffect;
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <SignalRContext.Provider
+            connectEnabled={true}
+            url={"https://localhost:7156/ws"}
+            dependencies={[]}
+            transport={1}
+        >
+            <App/>
+        </SignalRContext.Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
